9226f0d75e84d09d3e05ed1a23e662b7f35e5f8a
Armant Hitchcock <armant.hitchcock@gmail.com>
2020-04-14 23:10:37 +0200

    extra comment for app setup

--- helloWorldApp.py
+++ helloWorldApp.py
@@ -1,7 +1,10 @@
 from tkinter import *
+#Must be before code
 root = Tk()
 #Create a label Widget
 myLabel = Label(root, text="Hellow World!")
 #Shoving it into the screen
 myLabel.pack()
+
+#Must be last piece of code
 root.mainloop()
